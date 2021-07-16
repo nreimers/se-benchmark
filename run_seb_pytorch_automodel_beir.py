243210b9c4c9b223fa1def28474c4fb44bbaba9c
Trent <devtrentoh@gmail.com>
2021-07-15 20:00:48 -0700

    Asymmetric_mode fix

--- run_seb_pytorch_automodel_beir.py
+++ run_seb_pytorch_automodel_beir.py
@@ -29,6 +29,7 @@ def main(model_names, eval_datasets, sample_k=10, asymmetric_mode=False):
         corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 
         if asymmetric_mode:
+            logging.info("Using asymmetric mode")
             try:
                 assert len(model_names) == 2
                 q_model = model_names[0]
@@ -73,46 +74,47 @@ def main(model_names, eval_datasets, sample_k=10, asymmetric_mode=False):
                             rank + 1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
             except Exception as e:
                 print(e)
+        else:
+            logging.info("Using symmetric mode")
+            # Loading Models
+            for model_name in model_names:
+                logging.info(model_name)
+                try:
+                    word = models.Transformer(model_name)
+                    pool = models.Pooling(word.get_word_embedding_dimension())
+                    norm = models.Normalize()
 
-        # Loading Models
-        for model_name in model_names:
-            logging.info(model_name)
-            try:
-                word = models.Transformer(model_name)
-                pool = models.Pooling(word.get_word_embedding_dimension())
-                norm = models.Normalize()
+                    model = SentenceTransformer(modules=[word, pool, norm])
 
-                model = SentenceTransformer(modules=[word, pool, norm])
+                    beir_model = beir_models.SentenceBERT()
+                    beir_model.q_model = model
+                    beir_model.doc_model = model
 
-                beir_model = beir_models.SentenceBERT()
-                beir_model.q_model = model
-                beir_model.doc_model = model
+                    # Testing Models
+                    model = DRES(beir_model, batch_size=16)
+                    retriever = EvaluateRetrieval(model, score_function="cos_sim")
 
-                # Testing Models
-                model = DRES(beir_model, batch_size=16)
-                retriever = EvaluateRetrieval(model, score_function="cos_sim")
+                    results = retriever.retrieve(corpus, queries)
 
-                results = retriever.retrieve(corpus, queries)
+                    logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
+                    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
 
-                logging.info("Retriever evaluation for k in: {}".format(retriever.k_values))
-                ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+                    mrr = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="mrr")
+                    recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="recall_cap")
+                    hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
 
-                mrr = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="mrr")
-                recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="recall_cap")
-                hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
+                    query_id, ranking_scores = random.choice(list(results.items()))
+                    scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
+                    logging.info("Query : %s\n" % queries[query_id])
 
-                query_id, ranking_scores = random.choice(list(results.items()))
-                scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
-                logging.info("Query : %s\n" % queries[query_id])
-
-                for rank in range(sample_k):
-                    doc_id = scores_sorted[rank][0]
-                    # Format: Rank x: ID [Title] Body
-                    logging.info(
-                        "Rank %d: %s [%s] - %s\n" % (
-                            rank + 1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
-            except Exception as e:
-                print(e)
+                    for rank in range(sample_k):
+                        doc_id = scores_sorted[rank][0]
+                        # Format: Rank x: ID [Title] Body
+                        logging.info(
+                            "Rank %d: %s [%s] - %s\n" % (
+                                rank + 1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
+                except Exception as e:
+                    print(e)
 
 
 if __name__ == "__main__":
